37efc8789a839d206765d5a5e13f76564a093c5b
Ethan Dowler <ethan.dowler@daveramsey.com>
2020-03-25 20:52:44 -0500

    add current hp/damage taken to Enemy

--- src/components/Enemy/Enemy.js
+++ src/components/Enemy/Enemy.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import * as EnemyTypes from '../../data/EnemyTypes'
 
-const Enemy = ({ type }) => {
+const Enemy = ({ type, damageTaken = 0 }) => {
   const data = EnemyTypes[type]
   if (data === undefined) return <div>Unknown Enemy Type: {type}</div>
 
   return (
     <div className="Enemy">
       <div className="Enemy-name">{data.name}</div>
-      <div className="Enemy-health">Health: {data.health}</div>
+      <div className="Enemy-health">Health: {data.health - damageTaken}</div>
       <div className="Enemy-attack">Attack: {data.attack}</div>
+
     </div>
   )
 }
